459b9cf98c4d4d5028266df7e9cddd2fe470c628
healyr4 <healyr4@tcd.ie>
2020-12-05 00:04:15 +0000

    Update talker.py

--- Lab1/talker.py
+++ Lab1/talker.py
@@ -1,50 +1,32 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
+#1st line ensures script is executed as a Python script
+#Every Python ROS Node will have this declaration at the top
+
+# license removed for brevity
 
 ## Simple talker demo that published std_msgs/Strings messages
 ## to the 'chatter' topic
 
+#Needed when writing an ROS Node.
 import rospy
+#so we can reuse the std_msgs/String message type 
+#(a simple string container) for publishing. 
 from std_msgs.msg import String
 
 def talker():
+    #Define the talker's interface to the rest of ROS
+    #Declare node is publishing to the chatter topic
+    #using the message type String
+    #Queue # limits num messsages if not getting fast enough
     pub = rospy.Publisher('chatter', String, queue_size=10)
+    #Tell rospy the name of your node
+    #Anonymous eensures node has a unique name
     rospy.init_node('talker', anonymous=True)
+    #Rate object rate-- Go through loop 10/s
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
+        # Write string to screen, Node's log file & rosout
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
