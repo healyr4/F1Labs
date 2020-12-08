a68b2266e35b680c6ad49c18f48db873a6dcdd0c
healyr4 <healyr4@tcd.ie>
2020-12-08 18:29:23 +0000

    Update lidar_listener.py

--- Lab1/lidar_listener.py
+++ Lab1/lidar_listener.py
@@ -1,1 +1,32 @@
-#message
+#!/usr/bin/env python
+
+
+#float32 range_min        # minimum range value [m]
+#float32 range_max        # maximum range value [m]
+
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'Range', data.ranges)
+    #print(data.ranges)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    #Subscribe to the scan topic
+    rospy.Subscriber('scan', LaserScan, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
